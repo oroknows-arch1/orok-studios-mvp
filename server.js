09228b263b01808211f355267f597b5fba4483a8
“Sean <oroknows@gmail.com>
2026-04-09 19:41:40 +1000

    Fix duplicate app listen

--- server.js
+++ server.js
@@ -571,7 +571,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
